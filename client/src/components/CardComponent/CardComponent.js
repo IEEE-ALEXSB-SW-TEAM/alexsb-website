9d3a198736612bbd185d4d42598c4242ca7d7449
Tamim Sherif <101951441+Tamim21@users.noreply.github.com>
2024-03-20 17:34:41 +0200

    basic card compoenet +(Not Final) + Mt3mlsh Refactoring ya Ziyad

--- client/src/components/CardComponent/CardComponent.js
+++ client/src/components/CardComponent/CardComponent.js
@@ -3,30 +3,28 @@ import './CardComponent.css';
 *
 * @author Tamim Sherif <Tamim.Sherif@oultook.com>
 * @function CardComponent - This is a functional component that will be used to display the card component.
-* @param {string} title - N/A.
-* @param {string} description - N/A.
-* @param {string} image - N/A.
+* @param {string} title - title for the card.
+* @param {string} description - decritption for teh card.
+* @param {string} image - image for the card.
 * @returns {JSX} - It returns the JSX for the card component.
 * @summary - This is a functional component that will be used to display the card component.
 * @see - This component is being used in the Home component.
-* @todo - N/A.
-* @bug - N/A.
+* @todo - better hovering effect , dynamic alignment.
+* @bug - dimensions.
 * @note - N/A.
 */
-function CardComponent({ title, description, image }) {
+function CardComponent(props) {
   return (
-    <div className='card'>
+    <div className='card'>                                                                                                  
     <div className='card-image'>
-        <img src={'/images/image-1.jpg'} alt={title} />
-
+            <img src={props.image} alt="Mountains" /> 
     </div>
     <div className='card-title'>
-
+    <p><center>{props.title}</center></p>
     </div>
     <div className='card-description'>
-
+    <p><center>{props.description}</center></p>
     </div>
-
     </div>
   );
 }
