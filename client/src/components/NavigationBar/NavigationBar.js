63f6581ed0511f15cb34b74bbf5f234e55e0ccad
Ziyad Eslam <ziyadeslam@hotmail.com>
2024-03-20 02:40:53 +0200

    reworked navbar

--- client/src/components/NavigationBar/NavigationBar.js
+++ client/src/components/NavigationBar/NavigationBar.js
@@ -7,12 +7,23 @@ function NavigationBar() {
     const imageHeight = 10;
 
     const [click, setClick] = useState(false) //useState intialized by false and stored in click and setclick is function to change state
-    const handleClick = () => setClick(!click) //handleclick function inverts the last value of click
+    const handleClick = () => {
+        setClick(!click) //handleclick function inverts the last value of click
+        if (!click) {
+            setTransparency(false)
+        } else if (window.scrollY >= imageHeight) {
+            setTransparency(false)
+        } else {
+            setTransparency(true)
+        }
+    }
     const closeMenu = () => setClick(false) //CloseMenu function sets the click state by false
 
     const [Transparent, setTransparency] = useState(true)
     const hideNavbar = () => {
-        if (window.scrollY >= imageHeight) {
+        if (click) {
+            setTransparency(false)
+        } else if (window.scrollY >= imageHeight) {
             setTransparency(false)
         } else {
             setTransparency(true)
@@ -24,11 +35,11 @@ function NavigationBar() {
     return (
         <div className={Transparent ? 'header' : 'header header-bg'}>
             <nav className='navbar'>
-                <div><img src={logo} alt='logo' /></div>
-                    <div className='hamburger' onClick={handleClick}>
-                        {click ? (<FaTimes size={30} style={{ color: '#FFFFFF'}}/>)
-                            : (<FaBars size={30} style={{ color: '#FFFFFF' }} />)}
-                    </div>
+                <div><a href='/'><img src={logo} alt='logo' /></a></div>
+                <div className='hamburger' onClick={handleClick}>
+                    {click ? (<FaTimes size={30} style={{ color: '#FFFFFF' }} />)
+                        : (<FaBars size={30} style={{ color: '#FFFFFF' }} />)}
+                </div>
                 <ul className={click ? "nav-menu active" : "nav-menu"}>
                     <li className='nav-item'>
                         <a href='/' onClick={closeMenu}><b>Home</b></a>
