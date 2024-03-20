3458241fb41da919ebd967f43ddd4229b05f6941
m13salama <mohamedsalama236@gmail.com>
2024-03-20 21:55:43 +0200

    updated pics and moved navbar, footer to the routers

--- client/src/Routers.js
+++ client/src/Routers.js
@@ -2,16 +2,20 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './views/home.js';
 import Events from './views/events.js';
 import About from './views/about.js';
+import NavBar from './components/NavigationBar/NavigationBar.js';
+import Footer from './components/footer/footer.js';
 
 function Routers() {
   return (
     <Router>
       <div>
+        <div><NavBar /></div>
         <Switch>
           <Route exact path="/" component={Home} />
           <Route path="/about" component={About} />
           <Route path="/events" component={Events} />
         </Switch>
+        <div><Footer/></div>
       </div>
     </Router>
   );
--- client/src/views/home.js
+++ client/src/views/home.js
@@ -1,18 +1,14 @@
-import NavBar from '../components/NavigationBar/NavigationBar.js';
-import Footer from '../components/footer/footer.js';
 import ImageSlider from '../components/image-slider/ImageSlider';
 
 function home() {
     const slides = [
-        { url: "./images/image-1.jpg", title: "beach" },
-        { url: "./images/image-2.jpg", title: "boat" },
-        { url: "./images/image-3.jpg", title: "forest" },
-        { url: "./images/image-4.jpg", title: "city" },
-        { url: "./images/image-5.jpg", title: "italy" },
+        { url: "./images/ieee-1.jpg", title: "valeo visit all" },
+        { url: "./images/ieee-2.jpg", title: "valeo visit IEEE members" },
+        { url: "./images/ieee-3.jpg", title: "backend boot camp" },
+        { url: "./images/ieee-4.jpg", title: "tivac boot camp" },
     ];
     return(
         <div>
-            <div><NavBar /></div>
             <ImageSlider slides={slides} />
             <br/>
             <br/>
@@ -52,7 +48,6 @@ function home() {
             <h1> Home Page </h1>
             <h1> Home Page </h1>
             <h1> Home Page </h1>
-            <div><Footer/></div>
         </div>
     );
 }
