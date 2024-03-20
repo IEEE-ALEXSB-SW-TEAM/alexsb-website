0e859c21b34ad8f8385cc480926c0910bd056ac5
Tamim Sherif <101951441+Tamim21@users.noreply.github.com>
2024-03-20 14:31:50 +0200

    Card Structure 2.0 + adding JsDocs

--- client/src/components/CardComponent/CardComponent.js
+++ client/src/components/CardComponent/CardComponent.js
@@ -1,8 +1,32 @@
-
+import './CardComponent.css';
+/**
+*
+* @author Tamim Sherif <Tamim.Sherif@oultook.com>
+* @function CardComponent - This is a functional component that will be used to display the card component.
+* @param {string} title - N/A.
+* @param {string} description - N/A.
+* @param {string} image - N/A.
+* @returns {JSX} - It returns the JSX for the card component.
+* @summary - This is a functional component that will be used to display the card component.
+* @see - This component is being used in the Home component.
+* @todo - N/A.
+* @bug - N/A.
+* @note - N/A.
+*/
 function CardComponent({ title, description, image }) {
   return (
-    <div className="card">
-      
+    <div className='card'>
+    <div className='card-image'>
+        <img src={'/images/image-1.jpg'} alt={title} />
+
+    </div>
+    <div className='card-title'>
+
+    </div>
+    <div className='card-description'>
+
+    </div>
+
     </div>
   );
 }
