b69d2b531757b89184261155473b3c568efe09ba
m13salama <mohamedsalama236@gmail.com>
2024-03-20 22:47:32 +0200

    added js docs and made it responsive

--- client/src/components/purpose/PurposeComponent.js
+++ client/src/components/purpose/PurposeComponent.js
@@ -1,15 +1,31 @@
 import React from 'react';
-import './PurposeComponent.css'
-    
+import './PurposeComponent.css';
+
+/**
+ * React component representing the purpose for IEEE-AlexSB section.
+ * @returns {JSX.Element} The purpose component JSX.
+ * @example
+ * // Usage:
+ * import PurposeComponent from './PurposeComponent';
+ * 
+ * const App = () => {
+ *   return (
+ *     <div>
+ *       <PurposeComponent />
+ *     </div>
+ *   );
+ * }
+ */
+
 const PurposeComponent = () => {
   return (
     <div className="purpose-container">
       <h1>Our Purpose</h1>
-        <p>Fostering a community where </p>
-        <p>students engage in enriching activities</p>
-        <p>that bolster skills, relationships, and personal growth.</p >
+      <p>Fostering a community where </p>
+      <p>students engage in enriching activities</p>
+      <p>that bolster skills, relationships, and personal growth.</p>
     </div>
   );
-}
+};
 
 export default PurposeComponent;
