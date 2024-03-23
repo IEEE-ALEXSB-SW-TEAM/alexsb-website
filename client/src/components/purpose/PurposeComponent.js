83c5c770ef6aa7d44e2cfc4e674d1c116355a936
Ziyad Eslam <ziyadeslam@hotmail.com>
2024-03-23 23:08:55 +0200

    tweaked purpose

--- client/src/components/purpose/PurposeComponent.js
+++ client/src/components/purpose/PurposeComponent.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import './PurposeComponent.css';
+import React from "react";
+import "./PurposeComponent.css";
 
 /**
  * React component representing the purpose for IEEE-AlexSB section.
@@ -7,7 +7,7 @@ import './PurposeComponent.css';
  * @example
  * // Usage:
  * import PurposeComponent from './PurposeComponent';
- * 
+ *
  * const App = () => {
  *   return (
  *     <div>
@@ -19,11 +19,18 @@ import './PurposeComponent.css';
 
 const PurposeComponent = () => {
   return (
-    <div className="purpose-container">
-      <h1>Our Purpose</h1>
-      <p>Fostering a community where </p>
-      <p>students engage in enriching activities</p>
-      <p>that bolster skills, relationships, and personal growth.</p>
+    <div>
+      <div className="about-container">
+        <h1>What is IEEE</h1>
+        <p>IEEE is the world's largest technical professional organization</p>
+        <p>dedicated to advancing technology for the benefit of humanity.</p>
+      </div>
+      <div className="purpose-container">
+        <h1>Our Purpose</h1>
+        <p>Fostering a community where </p>
+        <p>students engage in enriching activities</p>
+        <p>that bolster skills, relationships, and personal growth.</p>
+      </div>
     </div>
   );
 };
