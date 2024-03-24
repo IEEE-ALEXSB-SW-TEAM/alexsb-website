7a1fbb1fab7c4110ff391757bac9f9f02707a90b
Tamim Sherif <101951441+Tamim21@users.noreply.github.com>
2024-03-24 16:22:27 +0200

    Added Dynamic Props variables to change the height,width & text position in the Card Component + changed text alignment in the student benfits

--- client/src/components/Benefits/Benefits.js
+++ client/src/components/Benefits/Benefits.js
@@ -18,16 +18,25 @@ const Benefits = () => {
           title="Step 1: Sign Up Easily"
           description="Create an account with just a few clicks to start your journey."
           image="https://images.rawpixel.com/image_800/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDI0LTAyL3Jhd3BpeGVsb2ZmaWNlMTVfY2xvc2UtdXBfcGhvdG9fb2ZfYV9tYW5faGFuZF9jaGVja2luZ19hY2NvdW50c19mYjM4YmJiYi0xZWZiLTQ3YTItYmYxZi0wM2VmZjZmNWVmNTZfMS5qcGc.jpg"
+          width="400"
+          height="400"
+          position="center"
         />
         <CardComponent
           title="Step 2: Discover Activities"
           description="Browse a catalog of events and join these that intrigue you."
           image="https://assets-global.website-files.com/611648b6262a801811180f6c/638e2a3491dfa730752a0a97_team-building-activities-main-image.jpg"
+          width= "400"
+          height= "400"
+          position="center"
         />
         <CardComponent
           title="Step 3: Build Connections"
           description="Meet peer, share experiences, and grow your network."
           image="https://images.spiceworks.com/wp-content/uploads/2021/12/14212019/effective_communication_in_the_workplace_5ce290ac1c679.jpg"
+          width="400"
+          height="400"
+          position="center"
         />
         </div>
       </div>
@@ -42,11 +51,17 @@ const Benefits = () => {
           title="Skills Development"
           description="Gain valuable life and professional skills outside the classroom."
           image="https://static.vecteezy.com/system/resources/previews/001/266/740/non_2x/two-people-working-on-laptops-in-an-office-free-photo.jpg"
+          width="400"
+          height="400"
+          position="left"
         />
         <CardComponent
           title="Networking Hub"
           description="Connect with peers, alumni, and professionals in various fields."
           image="https://images.unsplash.com/photo-1519389950473-47ba0277781c?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8b2ZmaWNlJTIwbGFwdG9wfGVufDB8fDB8fHww"
+          width="400"
+          height="400"
+          position="left"
         />
       </div>
     </div>
--- client/src/components/CardComponent/CardComponent.js
+++ client/src/components/CardComponent/CardComponent.js
@@ -3,27 +3,33 @@ import './CardComponent.css';
 *
 * @author Tamim Sherif <Tamim.Sherif@oultook.com>
 * @function CardComponent - This is a functional component that will be used to display the card component.
+* @param {string} height - height for the card.
+* @param {string} width - width for the card.
+* @param {string} position - position for the text in the card.
 * @param {string} title - title for the card.
 * @param {string} description - decritption for teh card.
 * @param {string} image - image for the card.
 * @returns {JSX} - It returns the JSX for the card component.
 * @summary - This is a functional component that will be used to display the card component.
-* @see - This component is being used in the Home component.
+* @see - This component is being used in the Benefits component.
 * @todo - better hovering effect , dynamic alignment.
 * @bug - dimensions.
 * @note - N/A.
 */
 function CardComponent(props) {
+  const COMPONENT_HEIGHT = Number(props.height);
+  const COMPONENT_WIDTH = Number(props.width);
+  const TEXT_POSITION = props.position;
   return (
-    <div className='card'>                                                                                                  
+    <div className='card' style={{ '--width': `${COMPONENT_WIDTH}px`, '--height': `${COMPONENT_HEIGHT}px` , '--position': `${TEXT_POSITION}` }}>                                                                                                  
     <div className='card-image'>
             <img src={props.image} alt="card" /> 
     </div>
     <div className='card-title'>
-    <h3><center>{props.title}</center></h3>
+    <h3>{props.title}</h3>
     </div>
-    <div className='card-description'>
-    <p><center>{props.description}</center></p>
+    <div className='card-description' >
+    <p>{props.description}</p>
     </div>
     </div>
   );
