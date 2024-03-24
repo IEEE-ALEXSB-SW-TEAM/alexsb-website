82ad9b2f63f5b7219a9043ebcd64046712ad0d7f
Ziyad Eslam <ziyadeslam@hotmail.com>
2024-03-24 14:58:38 +0200

    prototype loading screen

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,22 @@
-import './App.css';
-import Routers from './Routers.js'
+import "./App.css";
+import Routers from "./Routers.js";
+import NavBar from "./components/NavigationBar/NavigationBar.js";
+import Footer from "./components/footer/footer.js";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    setTimeout(() => {
+      var element = document.querySelector(".loader-container");
+      if (element) element.parentNode.removeChild(element);
+    }, 2000);
+  });
+
   return (
     <div className="App">
+      <NavBar />
       <Routers />
+      <Footer />
     </div>
   );
 }
