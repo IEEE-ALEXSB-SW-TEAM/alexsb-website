b1498c8a6ba4871dd3513a2ca9a48a34723f0136
m13salama <mohamedsalama236@gmail.com>
2024-05-11 06:27:28 +0300

    finished contact us page

---
+++ client/src/views/contact.js
@@ -0,0 +1,10 @@
+import ContactUs from '../components/contact/contact';
+
+function contact(){
+    return(
+        <div>
+            <ContactUs />
+        </div>
+    );
+}
+export default contact; 
