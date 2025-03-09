9a91fe7de5bcde2fe148125fc3c61e92be540b16
amrrkhaled <amr.khaledsaada@gmail.com>
2025-03-09 16:26:21 +0200

    chore(swagger): setup Swagger configuration

--- server/src/server.js
+++ server/src/server.js
@@ -1,10 +1,20 @@
-import express from 'express';
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import { swaggerUi, swaggerSpec } from "./config/swagger.js";
 
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
 const PORT = process.env.PORT || 5000;
 
-app.use(express.json());
-
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server running on port ${PORT}`);
+    console.log(`Swagger Docs available at: http://localhost:${PORT}/api-docs`);
 });
